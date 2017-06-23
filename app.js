5a1d519212afc94af05434448e97ae3cbf50254e
lakerswgq <lakerswgq@qq.com>
2017-06-23 10:29:31 +0800

    add sendFile and renderFile method for  http res using sendFile middleware

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const path = require("path");
 let Server = require("./index");
+
 
 let app = new Server();
 
+
+app.use(require("./middlewares/sendFile"));
 app.use(function (req, res, next) {
     req.name = "julien";
     next();
@@ -9,13 +13,16 @@ app.use(function (req, res, next) {
 
 app.use("/", function (req, res){
     console.log("name:", req.name);
-    res.writeHead(200);
-    res.end("index page");
+    res.renderFile(path.resolve(__dirname, "./views/index.html"));
 })
 
 app.use("/user", function (req, res, next){
     res.writeHead(200);
     res.end("profile page");
+})
+
+app.use("/download", function (req, res){
+    res.sendFile(path.resolve(__dirname, "./views/index.html"));
 })
 
 app.listen(8000);
